5c4170a64df54eafcbd461a972c39bde41a51cc8
jucasantos <juliadecarvalhosantos40@gmail.com>
2023-11-06 19:37:16 +0000

    aula2

---
+++ javascript-typescript/general-concepts/exercices/ex1-aula1.js
@@ -0,0 +1,33 @@
+let string1 =  "Julia"; //string
+
+console.log(string1);
+
+let age = 21; //number
+
+console.log(age);
+
+let estaEmpregado = "true"; //boolean
+
+console.log(estaEmpregado);
+
+let endereco = "";
+
+console.log(endereco);
+
+let nomeCompleto = "Julia Santos";
+
+console.log(nomeCompleto);
+
+let mensagem = "Hello World";
+
+console.log(mensagem);
+
+let string2 = `${nomeCompleto} ${mensagem}`; // template literals
+
+console.log(string2);
+
+let num1 = 7; 
+let num2 = 9;
+let valor = num1 + num2; //soma
+
+console.log(valor);
